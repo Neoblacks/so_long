c3c821553686039a48a8f393ac2517470c3dc8a8
Antoine MUGNIER <amugnier@e1r10p6.clusters.42paris.fr>
2022-12-11 20:56:32 +0100

    Les debuts

---
+++ Makefile
@@ -0,0 +1,47 @@
+NAME = so_long.a
+
+PATH_LIBFT = libft
+LIBFT = $(PATH_LIBFT)/libft.a
+
+PATH_MLX = mlx
+MLX = $(PATH_MLX)/libmlx.a -I $(PATH_MLX) -L $(PATH_MLX) -lmlx -lXext -lX11 -lm
+
+PATH_SRCS = srcs
+
+SRCS = $(PATH_SRCS)/main.c
+
+OBJS = $(SRCS:.c=.o)
+
+CC = cc
+
+CFLAGS = -Wall -Wextra -Werror -g3
+
+RM = rm -f
+
+all: $(LIBFT) $(MLX) $(NAME)
+
+$(NAME): $(OBJS) $(MLX)
+	$(CC) $(CFLAGS) -o $(NAME) $(OBJS) $(LIBFT) $(MLX)
+
+.c.o:
+	$(CC) $(CFLAGS) -c $< -o $(<:.c=.o) -I $(PATH_MLX)/mlx.h
+
+$(LIBFT):
+	$(MAKE) -C $(PATH_LIBFT)
+
+$(MLX):
+	$(MAKE) -C $(PATH_MLX)
+
+clean:
+	$(MAKE) -C $(PATH_LIBFT) clean
+	$(MAKE) -C $(PATH_MLX) clean
+	$(RM) $(OBJS)
+
+fclean: clean
+	$(MAKE) -C $(PATH_LIBFT) fclean
+	$(MAKE) -C $(PATH_MLX) clean
+	$(RM) $(NAME)
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ srcs/main.c
@@ -0,0 +1,76 @@
+#include "../mlx/mlx.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+typedef struct	s_vars {
+	void	*mlx;
+	void	*win;
+}				t_vars;
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
+int close(int keycode, t_vars *vars)
+{
+	if (keycode == 65307)
+	{
+		mlx_destroy_window(vars->mlx, vars->win);
+		exit(0);
+	}
+	return (0);
+}
+
+int print_key(int keycode, t_vars *vars)
+{
+	(void)vars;
+	printf("keycode: %d\n", keycode);
+	return (0);
+}
+
+int mouse_win(t_vars *vars, int x, int y)
+{
+	(void)vars;
+	printf("x: %d, y: %u\n", x, y);
+	return (0);
+}
+
+int	main(void)
+{
+	// void	*mlx;
+	// void	*mlx_win;
+	// int		width;
+	// int		height;
+	// t_data	img
+
+	// mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	// img.img = mlx_new_image(mlx, 1920, 1080);
+	// img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+	// 		&img.endian);
+
+	t_vars	vars;
+
+	vars.mlx = mlx_init();
+	vars.win = mlx_new_window(vars.mlx, 500, 500, "Hello world!");
+
+	mlx_hook(vars.win, 6, 1L<<6, mouse_win, &vars);
+	mlx_hook(vars.win, 2, 1L<<0, print_key, &vars);
+
+	mlx_hook(vars.win, 2, 1L<<0, close, &vars);
+	mlx_hook(vars.win, 17, 1L<<17, close, &vars);
+
+	mlx_loop(vars.mlx);
+}
