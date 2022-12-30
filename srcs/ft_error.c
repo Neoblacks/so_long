be3f0a297f2c04995d76b54bf8b65a9b3a95a65f
Neoblacks <antoine.mugnier035@gmail.com>
2022-12-30 22:53:32 +0000

    update

--- srcs/ft_error.c
+++ srcs/ft_error.c
@@ -6,7 +6,7 @@
 /*   By: amugnier <amugnier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/13 21:32:59 by amugnier          #+#    #+#             */
-/*   Updated: 2022/12/14 11:37:31 by amugnier         ###   ########.fr       */
+/*   Updated: 2022/12/30 21:12:58 by amugnier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int ft_error(char *str)
 	int i;
 
 	i = 0;
+	if (str == NULL)
+		return (0);
 	while (str[i] != '\0')
 	{
 		write(2, &str[i], 1);
